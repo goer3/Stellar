772fa1ce7238a861d390778f543662c89e38234d
况波 <kuangbo@onething.net>
2025-01-14 21:25:16 +0800

    角色API接口

--- server/api/v1/system_user.go
+++ server/api/v1/system_user.go
@@ -17,16 +17,28 @@ func GetSystemUserApiListHandler(ctx *gin.Context) {
 		response.FailedWithMessage("获取当前用户的角色信息失败")
 		return
 	}
-
 	// 如果角色是超级管理员，则返回所有 Api 列表
+	var systemApiList []string
 	if systemRoleId == common.SystemSuperAdministratorRoleId {
 		var systemApis []model.SystemApi
-		common.DB.Find(&systemApis)
+		if err := common.DB.Find(&systemApis).Error; err != nil {
+			response.FailedWithMessage("获取系统Api列表失败")
+			return
+		}
+		for _, systemApi := range systemApis {
+			systemApiList = append(systemApiList, systemApi.Path)
+		}
 	} else {
 		var systemRole model.SystemRole
-		common.DB.Preload("SystemApi").Where("id = ?", systemRoleId).First(&systemRole)
+		if err := common.DB.Preload("SystemApis").Where("id = ?", systemRoleId).First(&systemRole).Error; err != nil {
+			response.FailedWithMessage("获取系统角色信息失败")
+			return
+		}
+		for _, systemApi := range systemRole.SystemApis {
+			systemApiList = append(systemApiList, systemApi.Path)
+		}
 	}
-
-	// 从 Context 中获取当前用户信息
-	response.Success()
+	response.SuccessWithData(gin.H{
+		"list": systemApiList,
+	})
 }
